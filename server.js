04b50fd90c18e0e7c4bf079db7ace5477d0705e5
mfahadarif85 <mfahadarif@gmail.com>
2022-10-26 13:45:48 +0500

    mongodb atlas connected

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindandModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 //start server
 const port = process.env.PORT || 3000;
