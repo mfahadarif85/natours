b2f0cae5dbd6be53921ccb1d325341c604da54b5
mfahadarif85 <mfahadarif@gmail.com>
2022-11-20 20:36:22 +0500

    global error handling middleware & refactoring

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -26,10 +28,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cannot find ${req.originalUrl} on this server!`,
-  });
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
