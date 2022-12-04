24e596462e67a2510858c993e06893dfe783017f
mfahadarif85 <mfahadarif@gmail.com>
2022-12-04 18:01:30 +0500

    data sanitization

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const morgan = require('morgan');
 
 const rateLimit = require('express-rate-limit');
 
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -11,21 +16,39 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 // Global middlewares
+// Set Security HTTP headers
+app.use(helmet());
 
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
-  max: 2,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization again XSS
+app.use(xss());
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toLocaleDateString();
   next();
